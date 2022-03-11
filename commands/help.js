5b9b060a0a9445613e21a5d700b3ea51ef2840a9
capierc3 <capierc3@asu.edu>
2022-03-11 02:42:10 -0500

    checked other platform for code and username standards

--- commands/help.js
+++ commands/help.js
@@ -10,10 +10,10 @@ module.exports = {
             "All commands start with '!'\n" +
             "- add platform id\n" +
             "   - Adds a new user to the bot. Needs the platform and user id to work.\n" +
-            "   - platform: steam, xbl\n" +
+            "   - platform: steam, xbl, epic, psn, switch\n" +
             "   - id: \n" +
             "       - Steam: number found in url on rltracker website\n" +
-            "       - others just your username (i think)\n" +
+            "       - All other just your username\n" +
             "- rank user:\n" +
             "   - Shows the ranking of the main user, will update if data is old\n" +
             "   - user: user name, case matters.";
